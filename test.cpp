5cca6f779958fb2e0c08f58cc9797a892892feea
Tofudog <leodefarias25@gmail.com>
2025-01-29 10:59:03 -0500

    another test

--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    //message about what you want to get out of CS 370
-    string message; cin >> message;
-    cout << message << '\n';
-}
